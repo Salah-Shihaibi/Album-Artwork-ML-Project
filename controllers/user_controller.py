ef3a98a35bc02f25df75211f1899f0a9933faa8b
ed <edheyev@gmail.com>
2022-03-03 11:14:12 +0000

    refactor and working table creation with psycopg2. need to go through and remove code associated with sql-alchemy

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,4 +1,37 @@
 from flask_restful import Resource
+import psycopg2
+from psycopg2 import Error
+
+
+def create_tables():
+    try:
+        connection = psycopg2.connect(user = "postgres",
+                                    password="0000",
+                                    host="127.0.0.1",
+                                    port="5432",
+                                    database="album_cover_db"
+                                    )
+        cursor = connection.cursor()
+        #create new table
+        create_table_query = """
+        CREATE TABLE users (
+          username VARCHAR(50) PRIMARY KEY,
+          name VARCHAR(50) NOT NULL,
+          email VARCHAR(100) UNIQUE,
+          password VARCHAR(100) NOT NULL
+          );
+        """
+        cursor.execute(create_table_query)
+        connection.commit()
+        print("Table created successfully in PSQL")
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
 class Users(Resource):
         def get(self):
             return 200
@@ -6,4 +39,7 @@ class Users(Resource):
 
 class Psql(Resource):
         def get(self):
+            create_tables()
             return 200
+
+
--- factory/main_factory.py
+++ factory/main_factory.py
@@ -7,30 +7,15 @@ from factory.flask_setup import setup_api
 from factory.db_setup import setup_db
 #from db.seed_db import User
 
+
 def create_app():
     # app = setup_api()
     # db = setup_db()
     app = Flask(__name__)
-    app.config.from_object(DevelopmentConfig()) 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db = SQLAlchemy(app)
-    
-    class User(db.Model):
-        __tablename__='user'
-        id = db.Column(db.Integer, primary_key=True)
-        username = db.Column(db.String(80), unique=True, nullable=False)
-        email = db.Column(db.String(120), unique=True, nullable=False)
-
-        def __init__(self, id, username, email):
-            self.id = id
-            self.username = username
-            self.email = email
-            
-    User.create()
-    db.session.commit()
     api = Api(app)
      
     api.add_resource(Users, "/")
     api.add_resource(Psql, "/psql")
     return app
+
 
