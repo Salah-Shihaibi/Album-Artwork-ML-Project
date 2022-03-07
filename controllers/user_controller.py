c722c2161e4b37fac691d1c137716f5f21ff96b9
Eden O'Brien <eobr@Edens-MacBook-Air.local>
2022-03-07 14:09:34 +0000

    refactored error handling to class system

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,23 +1,23 @@
 from flask_restful import Resource
 from flask import jsonify, make_response, abort, request
 from error_handling.error_classes import PasswordTooShortError
+# from error_handling.error_classes import PasswordTooShortError
 from models.user_model import register_user, login_user
-
 
 class Ping(Resource):
     def get(self):
         return 200
 
-
 class Register(Resource):
     def post(self):
-        user = {'user': register_user(request.json)}
+        return PasswordTooShortError()
+        # user =  {'user':register_user(request.json)}
+        # print(user, type(user), "<<<<<<<<<<<<<")
         # if len(user['password']) < 5:
-        #raise PasswordTooShortError
-        return user, 200
-
+        # #     raise PasswordTooShortError
+        # return user, 200
 
 class Login(Resource):
     def post(self):
-        cred = {'user': login_user(request.json)}
+        cred = {'user':login_user(request.json)} 
         return cred, 200
--- error_handling/error_classes.py
+++ error_handling/error_classes.py
@@ -2,29 +2,40 @@
 
 # handle psql error
 
-# email address incorrect format,
-# username already in use,
+## email address incorrect format, 
+# username already in use, 
 # email address already in use
 # password not long enough
 from flask import make_response, abort, jsonify
+from flask_restful import Resource
+
+# class Error(Exception):
+#     pass
 
 
-class UsernameTakenError(Exception):
-    abort(make_response(jsonify({
-        "status": 400,
-        "msg": 'Username already exists.'
-    })), 400)
-
-
-class EmailTakenError(Exception):
-    abort(make_response(jsonify({
-        "status": 400,
-        "msg": 'Email already exists.'
-    })), 400)
-
+# class UsernameTakenError(Exception):
+#     def __init__(self):
+#         abort(jsonify({
+#             "status":400,
+#             "msg":'Username already exists.'
+#             }), 400)
 
 class PasswordTooShortError(Exception):
-    abort(make_response(jsonify({
-        "status": 400,
-        "msg": 'Password should be 5 characters or longer.'
-    })), 400)
+    def __new__(self):
+        self.msg = 'Password should be 5 characters or longer.'
+        self.status = 400
+        return {
+            "msg": self.msg,
+            "status": self.status
+            }, 400
+
+
+
+# class EmailTakenError(Exception):
+#     abort(make_response(jsonify({
+#         "status":400,
+#         "msg":'Email already exists.'
+#         })), 400)
+
+
+
