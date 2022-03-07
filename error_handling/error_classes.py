269a0bbd9e79c83fb41960f60031dbb5a39a6b2f
ed <edheyev@gmail.com>
2022-03-07 17:03:11 +0000

    refactored error classes

--- error_handling/error_classes.py
+++ error_handling/error_classes.py
@@ -1,16 +1,14 @@
-# does body contain correct dictionary format
-
 # handle psql error
 
-## email address incorrect format, 
-# username already in use, 
-# email address already in use
-# password not long enough
 from flask import make_response, abort, jsonify
 from flask_restful import Resource
 
-# class Error(Exception):
-#     pass
+class CustomError(Exception):
+    def __init__(self):
+        self.msg = 'Internal server error'
+        self.status_code = 500
+    def response(self):
+        return {"msg":self.msg, "status_code": self.status_code}, self.status_code 
 
 
 # class UsernameTakenError(Exception):
@@ -20,7 +18,7 @@ from flask_restful import Resource
 #             "msg":'Username already exists.'
 #             }), 400)
 
-class PasswordTooShortError(Exception):
+class PasswordTooShortError(CustomError):
     def __new__(self):
         self.msg = 'Password should be 5 characters or longer.'
         self.status = 400
@@ -35,33 +33,23 @@ class PasswordTooShortError(Exception):
 #         "msg":'Email already exists.'
 #         })), 400)
 
-
 # user does not exist
-class NoUserFoundError(Exception):
+class NoUserFoundError(CustomError):
     def __init__(self):
         self.msg = 'User/password combination is not valid.' 
         self.status_code = 400
-        
-    def response(self):
-        return {"msg":self.msg, "status_code": self.status_code}, self.status_code    
-    
+
 # user exists incorrect password
-class IncorrectPasswordError(Exception):
+class IncorrectPasswordError(CustomError):
     def __init__(self):
         self.msg = 'User/password combination is not valid.' 
         self.status_code = 400
         
-    def response(self):
-        return {"msg":self.msg, "status_code": self.status_code}, self.status_code    
-    
 # incorrect request body
-class IncorrectRequestBody(Exception):
+class IncorrectRequestBody(CustomError):
     def __init__(self):
         self.msg = 'Credentials invalid format' 
         self.status_code = 400
         
-    def response(self):
-        return {"msg":self.msg, "status_code": self.status_code}, self.status_code    
-    
-# error while connecting to db
+  
 
